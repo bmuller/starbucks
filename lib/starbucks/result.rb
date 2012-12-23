89190319594897c317acbb83c6def9b5224b79d6
Brian Muller <bamuller@gmail.com>
2012-12-23 14:28:10 -0500

    version 0.0.3, added miles_away calculation

--- lib/starbucks/result.rb
+++ lib/starbucks/result.rb
@@ -7,5 +7,11 @@ module Starbucks
     def name
       fetch("Name")
     end
+
+    def miles_away
+      meters = address.fetch("Distance")
+      # 1 mile = 1609.344 meters
+      meters.to_f / 1609.344
+    end
   end
 end
